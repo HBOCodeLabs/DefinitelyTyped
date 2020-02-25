c9a741c468adb1a0f2e58d26e2e665bdd9b54242
Kellen Peterson <kellen.peterson@gmail.com>
2020-02-25 12:21:34 -0800

    Revert "add(mustache): better mustache.parse return type (#42468)"
    This reverts commit 6dd7b4f116aea7e614a1d43728859934f53f034c.

--- types/node-vault/index.d.ts
+++ types/node-vault/index.d.ts
@@ -3,7 +3,7 @@
 // Definitions by: Jianrong Yu <https://github.com/YuJianrong>
 //                 Neil Bryson Cargamento <https://github.com/neilbryson>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 3.7
+// TypeScript Version: 2.3
 
 import * as mustache from "mustache";
 import * as request from "request";
